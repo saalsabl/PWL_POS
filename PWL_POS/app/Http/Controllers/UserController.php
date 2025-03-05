aa39ec71edcce25c4e5a9938637a1ddb51c2e23b
saalsabl <aisyahnzkk@gmail.com>
2025-03-05 11:05:41 +0700

    Praktikum 2.3

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index() 
     {
-        // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2) -> count();
+        // dd($user);
+        // $user = UserModel::all();
         return view('user', ['data' => $user]);
+
     }
 }
