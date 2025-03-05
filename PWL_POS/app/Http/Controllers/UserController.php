c5c5f230055bc76b58a0a15cee98df2b66e2e57a
saalsabl <aisyahnzkk@gmail.com>
2025-03-05 11:37:49 +0700

    Praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -11,10 +11,23 @@ class UserController extends Controller
 {
     public function index() 
     {
-        $user = UserModel::where('level_id', 2) -> count();
-        // dd($user);
-        // $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => hash::make('12345'),
+                'level_id' => 2
+            ],
 
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        );
+        $user->save();
+        return view('user', ['data' => $user]);
     }
 }
--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -7,18 +7,17 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th> --}}
-            <th>Jumlah Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
             <tr>
-                {{-- <td>{{ $data -> user_id}}</td>
+                <td>{{ $data -> user_id}}</td>
                 <td>{{ $data -> username}}</td>
-                <td>{{ $data -> nama}}</td> --}}
-                <td>{{ $data }}</td>
+                <td>{{ $data -> nama}}</td>
+                <td>{{ $data -> level_id }}</td>
             </tr>
-    </table>
     </table>
 </body>
 </html>
