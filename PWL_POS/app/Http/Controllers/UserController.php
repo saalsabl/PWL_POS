d58bb3823794587e0c48a6aade1145a50d36d22b
saalsabl <aisyahnzkk@gmail.com>
2025-03-05 09:52:26 +0700

    Praktikum 1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,15 +10,15 @@ class UserController extends Controller
 {
     public function index() {
         $data = [
-            'nama' => 'Pelanggan Pertama',
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
+            'level_id' => 2,
+            // 'username' => 'manager_dua',
+            // 'nama' => 'Manager 2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        // UserModel::insert($data);
-        UserModel::where('username', 'customer-1') -> update($data);
-
+        UserModel::create($data);
+    
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -10,8 +10,8 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_user';
-    protected  $primaryKey = 'user_id';
+    protected $primaryKey = 'user_id';
 
-    
+    protected $fillable = ['level_id', 'username', 'nama'];
 
 }
