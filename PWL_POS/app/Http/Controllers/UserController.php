4293eda0dc9a561054c47e1dfe30bab42da9f1d9
saalsabl <aisyahnzkk@gmail.com>
2025-03-05 10:11:43 +0700

    Praktikum 2.2 (yg sebelumnya typo hrs nya 2.1)

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,9 +10,8 @@ class UserController extends Controller
 {
     public function index() 
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
