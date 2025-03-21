686dd3e86ca2b9d6a436a46d0f9a16a76d1acdcf
saalsabl <aisyahnzkk@gmail.com>
2025-03-21 08:06:43 +0700

    Praktikum 2 (logo dashboard nya msh gaada)

--- PWL_POS/resources/views/layouts/sidebar.blade.php
+++ PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -1,98 +1,75 @@
     <div class="sidebar">
-      <!-- Sidebar user (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
       <!-- SidebarSearch Form -->
       <div class="form-inline">
         <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
+            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-sidebar">
+                  <i class="fas fa-search fa-fw"></i>
+                </button>
+              </div>                  
           </div>
-        </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+             with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="{{ url('/')}}" class="nav-link {{ ($activeMenu == 'dashboard') ? 'active' : '' }} ">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+
+              <li class="nav-header">Data Pengguna</li>
+              <li class="nav-item">
+                <a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level')? 'active' : '' }} ">
+                  <i class="nav-icon fas fa-layer-group"></i>
+                  <p>Level User</p>
+                </a>
+              </li>
+
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
+            <a href="{{ url('/user')}}" class="nav-link {{ ($activeMenu == 'user') ? 'active' : '' }}" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>Data User</p>
             </a>
-            <ul class="nav nav-treeview">
+          </li>
+          <li class="nav-header">Data Barang</li>
+          <li class="nav-item">
+            <a href="{{ url('/kategori')}}" class="nav-link {{ ($activeMenu == 'kategori') ? 'active' : '' }}" class="nav-link">
+              <i class="nav-icon far fa-bookmark"></i>
+              <p>Kategori Barang</p>
+            </a>
+          </li>
               <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                <a href="{{ url('/barang')}}" class="nav-link {{ ($activeMenu == 'barang') ? 'active' : '' }}" class="nav-link">
+                  <i class="nav-icon far fa-list-alt"></i>
+                  <p>Data Barang</p>
+                </a>
+              </li>
+              <li class="nav-header">Data Transaksi</li>
+              <li class="nav-item">
+                <a href="{{ url('/stok')}}" class="nav-link {{ ($activeMenu == 'stok') ? 'active' : '' }}" class="nav-link">
+                  <i class="nav-icon fas fa-cubes"></i>
+                  <p>Stok Barang</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../../index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../../index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                <a href="{{ url('/barang')}}" class="nav-link {{ ($activeMenu == 'barang') ? 'active' : '' }}" class="nav-link">
+                  <i class="nav-icon fas fa-cash-register"></i>
+                  <p>Transaksi Penjualan</p>
                 </a>
               </li>
             </ul>
-          </li>
-          <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-copy"></i>
-              <p>
-                Layout Options
-                <i class="fas fa-angle-left right"></i>
-                <span class="badge badge-info right">6</span>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="../layout/top-nav.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Top Navigation</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../layout/top-nav-sidebar.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Top Navigation + Sidebar</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../layout/boxed.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Boxed</p>
-                </a>
-              </li>
-              <li class="nav-item">
+          </nav>
+      </div>
+
+
+
+              {{-- <li class="nav-item">
                 <a href="../layout/fixed-sidebar.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Fixed Sidebar</p>
@@ -660,4 +637,4 @@
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
-    </div>
+    </div> --}}
