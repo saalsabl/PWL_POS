fc8092017c6969feeb5ea891dea33b3e32c6f3ab
saalsabl <aisyahnzkk@gmail.com>
2025-04-08 13:33:09 +0700

    Jobsheet 5

--- PWL_POS/resources/views/user/index.blade.php
+++ PWL_POS/resources/views/user/index.blade.php
@@ -12,24 +12,29 @@
         @if (session('error')) 
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
-        {{-- <div class="row">
+        <div class="row">
             <div class="col-md-12">
                 <div class="form-group row">
                     <label class="col-1 control-label col-form-label">Filter:</label>
                     <div class="col-3">
                         <select class="form-control" id="level_id" name="level_id" required>
-                            <option 
-                            
-        @endsession
-             --}}
-        
-        <div class="card-body">
-            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-            <thead>
-                <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
-            </thead>
-        </table>
-    </div>
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted"> Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>                  
+    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+        <thead>
+            <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
+        </thead>
+    </table>
+</div>
 </div>
 @endsection
 
@@ -44,7 +49,11 @@
                 ajax: {
                     "url": "{{ url('user/list') }}", 
                     "dataType": "json",
-                    "type": "POST"
+                    "type": "POST",
+                    // jobsheet 5 praktikum 4 modifikasi
+                    "data" : function (d) {
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns: [
                     {
@@ -77,6 +86,9 @@
                     searchable: false
                 }
             ]
+        });
+        $('#level_id').on('change', function() {
+            dataUser.ajax.reload();
         });
     });
 </script> 
