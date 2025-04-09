7c7aff48f67c967238ae9b73529f795c9e3d4898
saalsabl <aisyahnzkk@gmail.com>
2025-04-09 19:50:07 +0700

    Jobsheet6 Praktikum1

--- PWL_POS/resources/views/user/index.blade.php
+++ PWL_POS/resources/views/user/index.blade.php
@@ -6,9 +6,14 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
-        
+
+        <div class="card-body">
+        @if (session ('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>          
+        @endif
         @if (session('error')) 
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
@@ -23,19 +28,25 @@
                                 <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
                             @endforeach
                         </select>
-                        <small class="form-text text-muted"> Level Pengguna</small>
+                        <small class="form-text text-muted">Level Pengguna</small>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>                  
-    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-        <thead>
-            <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
+        </div>                 
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>Level Pengguna</th>
+                    <th>Aksi</th>
+                </tr>
         </thead>
     </table>
 </div>
 </div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css') 
@@ -43,9 +54,17 @@
 
 @push('js')
     <script>
+        function modalAction(url = ''){
+            $('#myModal').load(url,function(){
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                // serverSide: true, jika ingin menggunakan server side processing serverSide: true,
+            dataUser = $('#table_user').DataTable({
+                //serverSide: true, jika ingin menggunakan server side processing 
+                serverSide: true, 
                 ajax: {
                     "url": "{{ url('user/list') }}", 
                     "dataType": "json",
@@ -67,7 +86,8 @@
                     className: "",
                     // orderable: true, jika ingin kolom ini bisa diurutkan
                     orderable: true,
-                    // searchable: true, jika ingin kolom ini bisa dicari searchable: true
+                    // searchable: true, jika ingin kolom ini bisa dicari 
+                    searchable: true
                 },{
                     data: "nama",
                     className: "", 
