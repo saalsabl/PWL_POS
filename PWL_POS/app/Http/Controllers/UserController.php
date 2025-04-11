fe71f5d4bb2a775d9a56b15d4abf581332fd6a6d
saalsabl <aisyahnzkk@gmail.com>
2025-04-11 14:06:52 +0700

    Jobsheet 6 Praktikum 3

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -278,4 +278,25 @@ class UserController extends Controller
 
         return view('user.confirm_ajax', ['user' => $user]);
     }
+
+    public function delete_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status'  => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status'  => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
 }
