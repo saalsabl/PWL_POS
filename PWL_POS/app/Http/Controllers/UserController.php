59f02eaea81bda6ac866a91b0cb51311dd7979b1
saalsabl <aisyahnzkk@gmail.com>
2025-04-15 21:16:50 +0700

    Jobsheet 7 Praktikum 1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\Facades\DataTables;
 
+
 class UserController extends Controller
 {
     public function index(){
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+Route::pattern('id', '[0-9]+'); // artinya ketika ada paramater {id}, maka harus berupa angka
 
-Route::get('/', [WelcomeController::class,'index']);
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('logout', [AuthController::class, 'login'])->middleware('auth');
 
-// user
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('register_proses', [AuthController::class, 'register_proses']);
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/', [WelcomeController::class,'index']);
+
+    // user
 Route::group(['prefix' => 'user'], function(){
     Route::get('/', [UserController::class, 'index']);          // menampilkan halaman awal user
     Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
@@ -24,4 +36,5 @@ Route::group(['prefix' => 'user'], function(){
     Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);  // untuk tampilkan form confirm delete user Ajax
     Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // untuk hapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+    });
 });
